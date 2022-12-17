bc886fcc6a2c4da8bff147b18109607c5c630dad
Theo Zhang <azhang8@g.ucla.edu>
2022-12-17 14:23:35 -0800

    Adding search capability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
   const RESPONSE_TYPE = "token"
 
   const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+
 
   useEffect(() => {
     const hash = window.location.hash
@@ -31,14 +35,43 @@ function App() {
     window.localStorage.removeItem("token")
   }
 
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        params: {
+            q: searchKey,
+            type: "artist"
+        }
+    })
+
+    setArtists(data.artists.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Spotify React</h1>
+        <h1>abstractify</h1>
         {!token ?
             <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
                 to Spotify</a>
             : <button onClick={logout}>Logout</button>}
+        <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Search</button>
+        </form>
+        {renderArtists()}
       </header>
     </div>
   );
